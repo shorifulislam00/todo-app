4bc888bae524135e5473df268a528c21aa54d11d
Shoriful Islam <shorifulislam433@gmail.com>
2025-04-09 18:07:30 +0600

    add visualization

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use('/api/todos', require('./routes/todoRoutes'));
 
 app.listen(3000, () => console.log("Server running at http://127.0.0.1:3000"));
